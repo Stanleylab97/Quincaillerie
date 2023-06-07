9906c242f5c368425a09bc259c9d20a68c1df171
Stanleylab97 <stanleylab97@github.com>
2023-06-07 13:47:09 +0100

    Update host ip

--- lib/helper/networkHandler.dart
+++ lib/helper/networkHandler.dart
@@ -6,7 +6,7 @@ import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class NetworkHandler {
-  static String baseurl = "http://217.76.55.170:9094";
+  static String baseurl = "http://89.117.57.247:9094";
   var log = Logger();
 
   Future<http.Response> get(String url) async {
